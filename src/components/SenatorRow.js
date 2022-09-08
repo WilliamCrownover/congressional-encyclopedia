61f9c114efbfddaf1acca69adecb859902a413b7
William Crownover <wcrownover@aimconsulting.com>
2022-09-07 23:57:30 -0700

    Functionality to hide table and expand timeline

--- src/components/SenatorRow.js
+++ src/components/SenatorRow.js
@@ -2,11 +2,14 @@ import { useEffect, useState } from "react";
 import { dateFormat, daysInOffice, calcDaysBetween } from '../utils/timeUtils';
 import { bioImage } from '../utils/imageUtils';
 
-export const SenatorRow = ({allStates, bioguide, fullName, birthday, gender, terms, multipleSeats, wikipedia}) => {
+export const SenatorRow = ({allStates, bioguide, fullName, birthday, gender, terms, index, multipleSeats, wikipedia, hidden}) => {
 	const [image, setImage] = useState();
 	const firstTerm = terms[0];
 	const lastTerm = terms[terms.length - 1];
-	const firstCongressDate = '1789-03-04'
+	const firstCongressDate = '1789-03-04';
+	const hideSpacing = hidden ? 45 : 0;
+	const [windowWidth, setWindowWidth] = useState(document.documentElement.clientWidth);
+	const widthMultiply = hidden ? windowWidth/92000 : 0.013;
 	const cellWidthSmall = { width: '10px' };
 	const cellWidthDate = { width: '90px' };
 	const cellWidthDays = { width: '80px' };
@@ -17,81 +20,112 @@ export const SenatorRow = ({allStates, bioguide, fullName, birthday, gender, ter
 		const getData = async () => {
 			setImage(await bioImage(bioguide));
 		};
+		const handleResize = () => {
+			setWindowWidth(document.documentElement.clientWidth);
+		};
 
 		getData();
+		window.addEventListener('resize', handleResize);
 	}, [bioguide]);
+
+	const politicalPartyColor = (term) => {
+		return term.party ? term.party.replace('.', '').replaceAll(' ', '').toLowerCase() : 'blankparty';
+	}
+
+	const timelineLeftPosition = (term) => {
+		return calcDaysBetween(firstCongressDate, term.start)*widthMultiply;
+	}
+
+	const timelineTermWidth = (term) => {
+		return calcDaysBetween(term.start, term.end)*widthMultiply;
+	}
 
 	return (
 		<tr>
-			{/* Image */}
-			{!allStates &&
-				<td style={{ width: '10px' }}>
-					<img 
-						src={image} 
-						alt={bioguide} 
-						height='30' 
-						onError={({ currentTarget }) => {
-							currentTarget.onerror = null;
-							currentTarget.src="https://bioguide.congress.gov/2b2b2e5c1b613f0aeb70f77accc91781-190.wp.jpg";
-						}} 
-					/>
-				</td>
+			{!hidden && 
+				<>
+					{/* Image */}
+					{!allStates &&
+						<td style={{ width: '10px' }}>
+							<img 
+								src={image} 
+								alt={bioguide} 
+								height='30' 
+								onError={({ currentTarget }) => {
+									currentTarget.onerror = null;
+									currentTarget.src="https://bioguide.congress.gov/2b2b2e5c1b613f0aeb70f77accc91781-190.wp.jpg";
+								}} 
+							/>
+						</td>
+					}
+					{/* Name */}
+					<td style={cellWidthNames}>{fullName}</td>
+					{/* Birthday */}
+					<td style={cellWidthDate}>{birthday && dateFormat(birthday)}</td>
+					{/* Gender */}
+					<td style={cellWidthSmall} className={`${gender === 'M' ? 'male' : 'female'}`}>{gender}</td>
+					{/* State */}
+					<td style={cellWidthSmall}>{lastTerm.state}</td>
+					{/* Class */}
+					<td style={cellWidthSmall}>{lastTerm.class ? lastTerm.class : lastTerm.district}</td>
+					{/* Multiple Seats */}
+					<td style={cellWidthSmall} className={`${multipleSeats && 'multiseat'}`}>{multipleSeats && multipleSeats}</td>
+					{/* Party */}
+					<td style={cellWidthParty} className={`${lastTerm.party ? lastTerm.party.replace('.', '').replaceAll(' ', '').toLowerCase() : 'blankparty'}`} >{lastTerm.party}</td>
+					{/* Term Start */}
+					<td style={cellWidthDate}>{dateFormat(firstTerm.start)}</td>
+					{/* Age at Term Start */}
+					<td style={cellWidthSmall}>{birthday && (calcDaysBetween(birthday, firstTerm.start) / 365).toFixed()}</td>
+					{/* Term End */}
+					<td style={cellWidthDate}>{dateFormat(lastTerm.end)}</td>
+					{/* Age at Term End */}
+					<td style={cellWidthSmall}>{birthday && (calcDaysBetween(birthday, lastTerm.end) / 365).toFixed()}</td>
+					{/* Number of Terms */}
+					<td style={cellWidthSmall}>{terms.length}</td>
+					{/* Days Served */}
+					<td style={cellWidthDays}>{`${daysInOffice(terms)} Days`}</td>
+					{/* Wikipedia Link */}
+					<td>
+						<a href={wikipedia} target="_blank" rel="noreferrer">Wiki</a>
+					</td>
+				</>
 			}
-			{/* Name */}
-			<td style={cellWidthNames}>{fullName}</td>
-			{/* Birthday */}
-			<td style={cellWidthDate}>{birthday && dateFormat(birthday)}</td>
-			{/* Gender */}
-			<td style={cellWidthSmall} className={`${gender === 'M' ? 'male' : 'female'}`}>{gender}</td>
-			{/* State */}
-			<td style={cellWidthSmall}>{lastTerm.state}</td>
-			{/* Class */}
-			<td style={cellWidthSmall}>{lastTerm.class ? lastTerm.class : lastTerm.district}</td>
-			{/* Multiple Seats */}
-			<td style={cellWidthSmall} className={`${multipleSeats && 'multiseat'}`}>{multipleSeats && multipleSeats}</td>
-			{/* Party */}
-			<td style={cellWidthParty} className={`${lastTerm.party ? lastTerm.party.replace('.', '').replaceAll(' ', '').toLowerCase() : 'blankparty'}`} >{lastTerm.party}</td>
-			{/* Term Start */}
-			<td style={cellWidthDate}>{dateFormat(firstTerm.start)}</td>
-			{/* Age at Term Start */}
-			<td style={cellWidthSmall}>{birthday && (calcDaysBetween(birthday, firstTerm.start) / 365).toFixed()}</td>
-			{/* Term End */}
-			<td style={cellWidthDate}>{dateFormat(lastTerm.end)}</td>
-			{/* Age at Term End */}
-			<td style={cellWidthSmall}>{birthday && (calcDaysBetween(birthday, lastTerm.end) / 365).toFixed()}</td>
-			{/* Number of Terms */}
-			<td style={cellWidthSmall}>{terms.length}</td>
-			{/* Days Served */}
-			<td style={cellWidthDays}>{`${daysInOffice(terms)} Days`}</td>
-			{/* Wikipedia Link */}
-			<td>
-				<a href={wikipedia} target="_blank" rel="noreferrer">Wiki</a>
-			</td>
-			
+
 			{/* Timeline */}
-			<div style={{ display: 'flex', position: 'relative'}}>
+			<div className={'timelineContainer'} >
 				{terms.map( (term) => (
 					<div key={`${term.state}${term.class}${term.start}`}>
+						{/* Guidelines */}
 						<div
+							className={'timelineBlock'}
 							style={{ 
-								position: 'absolute', 
-								left: `0px`,
-								width: `${calcDaysBetween(firstCongressDate, term.start)/100}px`,
-								height: '32px',
+								left: `${hideSpacing}px`,
+								width: `${timelineLeftPosition(term)}px`,
 								borderTop: '1px solid lightgrey',
-								borderBottom: '1px solid lightgrey'
+								borderBottom: '1px solid lightgrey',
+								top: `${hidden ? '-35px' : '0px'}`
 							}}
 						/>
+						{/* Rep Term Individual */}
 						<div 
-							
-							className={`alternatingColors ${term.party ? term.party.replace('.', '').replaceAll(' ', '').toLowerCase() : 'blankparty'}`}
-							style={{ 
-								position: 'absolute', 
-								left: `${calcDaysBetween(firstCongressDate, term.start)/100}px`,
-								width: `${calcDaysBetween(term.start, term.end)/100}px`,
-								height: '32px'
+							className={`timelineBlock ${politicalPartyColor(term)}`}
+							style={{  
+								left: `${timelineLeftPosition(term) + hideSpacing}px`,
+								width: `${timelineTermWidth(term)}px`,
+								top: `${hidden ? '-35px' : '0px'}`
 							}}
-						/>	
+						/>
+						{/* Seat Terms Combined */}
+						{!hidden &&	
+							<div 
+								className={`timelineBlock ${politicalPartyColor(term)}`}
+								style={{  
+									left: `${timelineLeftPosition(term)}px`,
+									width: `${timelineTermWidth(term)}px`,
+									top: `-${(index + 1) * 35}px`,
+								}}
+							/>
+						}	
 					</div>					
 				))}
 			</div>
--- src/pages/Representatives.js
+++ src/pages/Representatives.js
@@ -9,6 +9,7 @@ export const Representatives = () => {
 	const { state } = useParams();
 	const allStates = state.toUpperCase() === 'ALL';
 	const [houseData, setHouseData] = useState([]);
+	const hide = true;
 
 	useEffect(() => {
 		const getData = async () => {
@@ -31,27 +32,33 @@ export const Representatives = () => {
 			<h2>All Current and Past U.S. Representatives {!allStates && `from ${statesList[state.toUpperCase()]}`} </h2>
 			
 			{Object.keys(houseData).map( (seat) => (
-				<table key={seat} style={{ width: '1170px'}}>
+				<table key={seat} style={{ width: `${hide ? '45px' : '1170px'}`}}>
 					<thead>
 						<tr>
-							{!allStates && <th>Image</th>}
-							<th>Name</th>
-							<th>Birthday</th>
-							<th>Gender</th>
-							<th>State</th>
-							<th>Class/ District</th>
-							<th>Multiple Seats</th>
-							<th>Party</th>
-							<th>Term Start Date</th>
-							<th>Age, Term Start</th>
-							<th>Term End Date</th>
-							<th>Age, Term End</th>
-							<th>Num of Terms</th>
-							<th>Days in Office</th>
-							<th>Wiki</th>
+							{hide ? 
+								<th>{seat}</th>
+							: 
+								<>
+									{!allStates && <th>Image</th>}
+									<th>Name</th>
+									<th>Birthday</th>
+									<th>Gender</th>
+									<th>State</th>
+									<th>Class/ District</th>
+									<th>Multiple Seats</th>
+									<th>Party</th>
+									<th>Term Start Date</th>
+									<th>Age, Term Start</th>
+									<th>Term End Date</th>
+									<th>Age, Term End</th>
+									<th>Num of Terms</th>
+									<th>Days in Office</th>
+									<th>Wiki</th>
+								</>
+							}
 						</tr>
 					</thead>
-					{houseData[seat].map( (rep) => (
+					{houseData[seat].map( (rep, index) => (
 						<tbody key={seat+rep.id.bioguide}>
 							<SenatorRow
 								allStates={allStates}
@@ -66,8 +73,10 @@ export const Representatives = () => {
 								birthday={rep.bio.birthday}
 								gender={rep.bio.gender}
 								terms={rep.terms}
+								index={index}
 								multipleSeats={rep.multipleSeats}
 								wikipedia={rep.id.wikipedia && createWikipediaURL(rep.id.wikipedia)}
+								hidden={hide}
 							/>
 						</tbody>
 					))}
