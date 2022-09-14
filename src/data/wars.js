6d735b48cbc2372c5004be6ebb7f62f9b6d20aa7
William Crownover <williamcrownover1@gmail.com>
2022-09-14 15:40:19 -0700

    US war data

---
+++ src/data/wars.js
@@ -0,0 +1,520 @@
+export const typicalWars = [
+	{
+		name: 'War of 1812',
+		start: '1812-06-18',
+		end: '1815-02-17'
+	},
+	{
+		name: 'Texas Revolution',
+		start: '1835-10-02',
+		end: '1836-04-21'
+	},
+	{
+		name: 'Mexican–American War',
+		start: '1846-04-25',
+		end: '1848-02-02'
+	},
+	{
+		name: 'American Civil War',
+		start: '1861-04-12',
+		end: '1865-05-26'
+	},
+	{
+		name: 'Spanish–American War',
+		start: '1898-04-21',
+		end: '1898-08-13'
+	},
+	{
+		name: 'World War I',
+		start: '1917-04-06',
+		end: '1918-11-11'
+	},
+	{
+		name: 'World War II',
+		start: '1941-12-11',
+		end: '1945-09-02'
+	},
+	{
+		name: 'Korean War',
+		start: '1950-06-25',
+		end: '1953-07-27'
+	},
+	{
+		name: 'Vietnam War',
+		start: '1955-11-01',
+		end: '1975-04-30'
+	},
+	{
+		name: 'Gulf War',
+		start: '1990-08-02',
+		end: '1991-02-28'
+	},
+	{
+		name: 'War in Afghanistan',
+		start: '2001-10-07',
+		end: '2021-08-30'
+	},
+	{
+		name: 'Iraq War',
+		start: '2003-03-20',
+		end: '2011-12-15'
+	},
+];
+
+export const americanIndianWars = [
+	{
+		name: 'Cherokee-American Wars',
+		// Actual start 1776-06-01
+		start: '1789-03-04',
+		end: '1794-11-07'
+	},
+	{
+		name: 'Northwest Indian War',
+		// Actual start 1786-01-01
+		start: '1789-03-04',
+		end: '1795-06-24'
+	},
+	{
+		name: "Tecumseh's War",
+		start: '1810-08-01',
+		end: '1813-10-05'
+	},
+	{
+		name: 'Creek War',
+		start: '1813-07-22',
+		end: '1814-08-09'
+	},
+	{
+		name: 'First Seminole War',
+		start: '1817-01-01',
+		end: '1818-01-01'
+	},
+	{
+		name: 'Arikara War',
+		start: '1823-06-02',
+		end: '1823-08-11'
+	},
+	{
+		name: 'Winnebago War',
+		start: '1827-06-27',
+		end: '1827-09-27'
+	},
+	{
+		name: 'Black Hawk War',
+		start: '1832-04-06',
+		end: '1832-08-27'
+	},
+	{
+		name: 'Second Seminole War',
+		start: '1835-12-23',
+		end: '1842-08-14'
+	},
+	{
+		name: 'Comanche Wars',
+		start: '1836-05-01',
+		end: '1875-07-02'
+	},
+	{
+		name: 'Cayuse Wars',
+		start: '1847-11-30',
+		end: '1855-06-01'
+	},
+	{
+		name: 'Apache Wars',
+		start: '1849-01-01',
+		end: '1924-01-01'
+	},
+	{
+		name: 'Navajo Wars',
+		start: '1849-08-29',
+		end: '1866-01-01'
+	},
+	{
+		name: 'Puget Sound War',
+		start: '1855-10-01',
+		end: '1856-03-10'
+	},
+	{
+		name: 'Rogue River Wars',
+		start: '1855-10-08',
+		end: '1856-06-01'
+	},
+	{
+		name: 'Third Seminole War',
+		start: '1855-12-20',
+		end: '1858-05-08'
+	},
+	{
+		name: 'Yakima War',
+		start: '1855-09-20',
+		end: '1858-09-23'
+	},
+	{
+		name: 'Paiute War',
+		start: '1860-05-06',
+		end: '1860-08-01'
+	},
+	{
+		name: 'Yavapai Wars',
+		start: '1861-01-01',
+		end: '1875-02-27'
+	},
+	{
+		name: 'Dakota War of 1862',
+		start: '1862-08-18',
+		end: '1862-09-26'
+	},
+	{
+		name: 'Colorado War',
+		start: '1864-04-12',
+		end: '1865-12-01'
+	},
+	{
+		name: 'Snake War',
+		start: '1864-01-01',
+		end: '1868-01-01'
+	},
+	{
+		name: 'Powder River Expedition',
+		start: '1865-07-01',
+		end: '1865-10-04'
+	},
+	{
+		name: "Red Cloud's War",
+		start: '1866-07-17',
+		end: '1868-05-07'
+	},
+	{
+		name: 'Comanche Campaign',
+		start: '1867-01-01',
+		end: '1875-01-01'
+	},
+	{
+		name: 'Modoc War',
+		start: '1872-07-06',
+		end: '1873-06-04'
+	},
+	{
+		name: 'Red River War',
+		start: '1874-06-27',
+		end: '1875-06-01'
+	},
+	{
+		name: 'Great Sioux War of 1876',
+		start: '1876-02-08',
+		end: '1877-05-05'
+	},
+	{
+		name: "Buffalo Hunters' War",
+		start: '1877-02-01',
+		end: '1877-03-18'
+	},
+	{
+		name: "Nez Perce War",
+		start: '1877-06-01',
+		end: '1877-10-01'
+	},
+	{
+		name: "Bannock War",
+		start: '1878-06-08',
+		end: '1878-08-01'
+	},
+	{
+		name: "Northern Cheyenne Exodus",
+		start: '1878-09-01',
+		end: '1879-04-01'
+	},
+	{
+		name: "Sheepeater Indian War",
+		start: '1879-05-31',
+		end: '1879-10-01'
+	},
+	{
+		name: "Victorio's War",
+		start: '1879-09-01',
+		end: '1880-10-01'
+	},
+	{
+		name: 'White River War',
+		start: '1879-09-29',
+		end: '1882-04-28'
+	},
+	{
+		name: 'Crow War',
+		start: '1887-09-30',
+		end: '1887-11-04'
+	},
+	{
+		name: 'Ghost Dance War',
+		start: '1890-12-29',
+		end: '1891-01-15'
+	},
+	{
+		name: 'Yaqui Wars',
+		start: '1896-08-12',
+		end: '1918-01-01'
+	},
+	{
+		name: 'Crazy Snake Rebellion',
+		start: '1909-03-01',
+		end: '1909-03-27'
+	},
+	{
+		name: 'Bluff War',
+		start: '1914-03-01',
+		end: '1915-03-11'
+	},
+	{
+		name: 'Posey War',
+		start: '1923-03-20',
+		end: '1923-03-23'
+	},
+];
+
+export const miscWars = [
+	{
+		name: 'First Barbary War',
+		start: '1801-05-10',
+		end: '1805-06-10'
+	},
+	{
+		name: 'Second Barbary War',
+		start: '1815-06-17',
+		end: '1815-06-19'
+	},
+	{
+		name: 'Aroostook War',
+		start: '1839-02-01',
+		end: '1839-03-21'
+	},
+	{
+		name: 'Second Opium War',
+		start: '1856-10-08',
+		end: '1860-10-24'
+	},
+	{
+		name: 'Utah War',
+		start: '1857-03-01',
+		end: '1858-07-01'
+	},
+	{
+		name: 'Reform War',
+		start: '1858-01-01',
+		end: '1866-01-01'
+	},
+	{
+		name: 'Pig War (1859)',
+		start: '1859-06-15',
+		end: '1859-10-01'
+	},
+	{
+		name: 'Cortina Troubles',
+		start: '1859-07-13',
+		end: '1861-05-01'
+	},
+	{
+		name: 'United States expedition to Korea',
+		start: '1871-06-01',
+		end: '1871-07-03'
+	},
+	{
+		name: 'Egyptian Expedition',
+		start: '1882-06-01',
+		end: '1882-07-01'
+	},
+	{
+		name: 'Bering Sea Anti-Poaching Operations',
+		start: '1891-06-22',
+		end: '1891-10-05'
+	},
+	{
+		name: 'Garza Revolution',
+		start: '1891-09-15',
+		end: '1893-03-01'
+	},
+	{
+		name: 'Second Samoan Civil War',
+		start: '1898-03-30',
+		end: '1899-12-02'
+	},
+	{
+		name: 'Philippine–American War',
+		start: '1899-02-04',
+		end: '1902-07-02'
+	},
+	{
+		name: 'Moro Rebellion',
+		start: '1899-02-04',
+		end: '1913-06-15'
+	},
+	{
+		name: 'Boxer Rebellion',
+		start: '1899-10-18',
+		end: '1901-09-07'
+	},
+	{
+		name: 'Mexican Border War',
+		start: '1911-04-07',
+		end: '1919-06-16'
+	},
+	{
+		name: 'Little Race War',
+		start: '1912-05-20',
+		end: '1912-07-01'
+	},
+	{
+		name: 'United States occupation of Nicaragua',
+		start: '1912-01-01',
+		end: '1933-01-02'
+	},
+	{
+		name: 'United States occupation of Veracruz',
+		start: '1914-04-21',
+		end: '1914-11-23'
+	},
+	{
+		name: 'United States occupation of Haiti',
+		start: '1915-07-28',
+		end: '1934-08-01'
+	},
+	{
+		name: 'United States occupation of Dominican Republic',
+		start: '1916-05-05',
+		end: '1924-12-27'
+	},
+	{
+		name: 'Russian Civil War',
+		start: '1918-01-12',
+		end: '1920-09-01'
+	},
+	{
+		name: 'Laotian Civil War',
+		start: '1959-05-23',
+		end: '1975-12-02'
+	},
+	{
+		name: 'Lebanon Crisis',
+		start: '1958-07-15',
+		end: '1958-10-25'
+	},
+	{
+		name: 'Bay of Pigs Invasion',
+		start: '1961-04-17',
+		end: '1961-04-20'
+	},
+	{
+		name: 'Dominican Civil War',
+		start: '1965-04-29',
+		end: '1965-05-26'
+	},
+	{
+		name: 'Korean DMZ Conflict',
+		start: '1966-10-05',
+		end: '1969-12-03'
+	},
+	{
+		name: 'Cambodian Civil War',
+		start: '1967-03-11',
+		end: '1975-04-17'
+	},
+	{
+		name: 'Multinational Force in Lebanon',
+		start: '1982-08-25',
+		end: '1984-03-31'
+	},
+	{
+		name: 'United States invasion of Grenada',
+		start: '1983-10-25',
+		end: '1983-10-29'
+	},
+	{
+		name: 'United States bombing of Libya',
+		start: '1986-04-15',
+		end: '1986-04-15'
+	},
+	{
+		name: 'Tanker War',
+		start: '1987-07-24',
+		end: '1988-08-04'
+	},
+	{
+		name: 'United States invasion of Panama',
+		start: '1989-12-20',
+		end: '1989-01-31'
+	},
+	{
+		name: 'Iraqi No-Fly Zone Enforcement Operations',
+		start: '1991-03-01',
+		end: '2003-03-20'
+	},
+	{
+		name: 'Operation Gothic Serpent',
+		start: '1993-08-22',
+		end: '1993-10-13'
+	},
+	{
+		name: 'Bosnian War',
+		start: '1992-04-06',
+		end: '1995-12-14'
+	},
+	{
+		name: 'Croatian War',
+		start: '1991-03-31',
+		end: '1995-11-12'
+	},
+	{
+		name: 'Operation Uphold Democracy',
+		start: '1994-09-19',
+		end: '1995-03-31'
+	},
+	{
+		name: 'Kosovo War',
+		start: '1998-02-28',
+		end: '1999-07-11'
+	},
+	{
+		name: 'American intervention in Yemen',
+		start: '2002-11-05',
+		end: '2022-09-13'
+	},
+	{
+		name: 'American intervention in the War in North-West Pakistan',
+		start: '2004-06-18',
+		end: '2018-07-04'
+	},
+	{
+		name: 'Second U.S. Intervention in the Somali Civil War',
+		start: '2007-01-07',
+		end: '2022-09-13'
+	},
+	{
+		name: 'Operation Ocean Shield',
+		start: '2009-08-17',
+		end: '2016-11-24'
+	},
+	{
+		name: 'International intervention in Libya',
+		start: '2011-03-19',
+		end: '2011-10-31'
+	},
+	{
+		name: 'Operation Observant Compass',
+		start: '2011-01-01',
+		end: '2017-03-01'
+	},
+	{
+		name: 'American-led intervention in Iraq',
+		start: '2014-06-15',
+		end: '2021-12-09'
+	},
+	{
+		name: 'American-led intervention in Syria',
+		start: '2014-09-22',
+		end: '2022-09-13'
+	},
+	{
+		name: 'American intervention in Libya',
+		start: '2015-11-13',
+		end: '2019-10-30'
+	},
+];
